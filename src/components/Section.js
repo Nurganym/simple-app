92317f42b8ece7fce694426351df5dfa36069f32
Nurganym Zholdosh <nurganym14@mail.com>
2022-04-11 10:45:52 +0600

    Main page styled

---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/Section.scss';
+
+const Section = () => {
+    return (
+       <section className="main-section">
+         <div className="content">
+             <h1>Propel your business.</h1>
+             <p>Propel comes with everything you need to get your business rolling.</p>
+             <button>Get Started</button>
+         </div>
+       </section>
+    )
+}
+
+export default Section
